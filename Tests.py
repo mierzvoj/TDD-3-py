b864c09637d6b5b0d3ae9e5d924af49e0095a608
wojciech mierzejewski <55141520+mierzvoj@users.noreply.github.com>
2022-12-07 17:18:52 +0100

    test no. 2

--- Tests.py
+++ Tests.py
@@ -7,9 +7,9 @@ class RomanNumeralsTest(unittest.TestCase):
     def test_1_is_a_single_i(self):
         self.assertEqual(roman(1), "I")
 
-    # def test_2_is_two_i_s(self):
-    #     self.assertEqual(roman(2), "II")
-    #
+    def test_2_is_two_i_s(self):
+        self.assertEqual(roman(2), "II")
+
     # def test_3_is_three_i_s(self):
     #     self.assertEqual(roman(3), "III")
     #
